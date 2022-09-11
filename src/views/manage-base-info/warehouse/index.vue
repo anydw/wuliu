ea26c1eb3d9e7577d3454d3920e46f6918c291cb
邓文 <969631494@qq.com>
2022-09-11 09:44:51 +0800

    1

--- src/views/manage-base-info/warehouse/index.vue
+++ src/views/manage-base-info/warehouse/index.vue
@@ -1,7 +1,14 @@
 <template>
   <div>
     <div class="top_bar">
-      1
+      <div>
+        <span>仓库编号</span>
+        <el-input v-model="input" placeholder="请输入内容" />
+      </div>
+      <div>
+        <span>仓库名称</span>
+        <el-input v-model="input" placeholder="请输入内容" />
+      </div>
     </div>
   </div>
 </template>
@@ -12,7 +19,7 @@ export default {
   components: {},
   data() {
     return {
-
+      input: ''
     }
   },
   computed: {},
@@ -29,5 +36,13 @@ export default {
 }
 </script>
 <style scoped lang="scss">
-
+.top_bar{
+  width: 1080px;
+  height: 127px;
+  background-color: pink;
+  margin: 30px;
+  ::v-deep.el-input__inner{
+    width: 223px;
+  }
+}
 </style>
